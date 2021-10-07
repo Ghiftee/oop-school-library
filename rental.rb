8a7d95ce7bbfe4bb963d5bafcd23d1138f2f5d34
Ghiftee <gigibetine@gmail.com>
2021-10-07 15:22:10 +0100

    Create relationship between book and rentals

--- rental.rb
+++ rental.rb
@@ -1,7 +1,18 @@
 class Rental
   attr_accessor :date
+  attr_reader :book, :person
 
   def initialize(date)
     @date = date
+  end
+
+  def book=(book)
+    @book = book
+    book.rentals << self unless book.rentals.include?(self)
+  end
+
+  def persons=(person)
+    @person = person
+    person.rentals << self unless person.rentals.include?(self)
   end
 end
