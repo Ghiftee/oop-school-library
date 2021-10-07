c6d4a54ccc46d3eb6aee5242dbffb3bebd7519c2
Ghiftee <gigibetine@gmail.com>
2021-10-07 07:47:33 +0100

    Fix rubocop errors

--- corrector.rb
+++ corrector.rb
@@ -1,7 +1,6 @@
 class Corrector
   def correct_name(name)
     capital_name = name.capitalize
-    sliced_name = capital_name.slice(0, 10)
-    sliced_name
+    capital_name.slice(0, 10)
   end
 end
