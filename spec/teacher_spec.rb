18aa9a2b5c430b96a9ce63ea49c2013e422d5639
Ghiftee <gigibetine@gmail.com>
2021-10-15 13:44:14 +0100

    Fix Rubocop errors

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,17 +1,17 @@
-require_relative "required_files"
+require_relative 'required_files'
 describe Teacher do
   before :each do
-    @teacher = Teacher.new(age: 50, name: "Jim", specialization: "English")
+    @teacher = Teacher.new(age: 50, name: 'Jim', specialization: 'English')
   end
-  describe "#instance" do
-    context "#new teacher" do
-      it "should return an instance of teacher" do
+  describe '#instance' do
+    context '#new teacher' do
+      it 'should return an instance of teacher' do
         expect(@teacher).to be_an_instance_of Teacher
       end
     end
-    context "#can_use_services?" do
-      it "should use services" do
-       expect(@teacher.can_use_services?).to be_truthy
+    context '#can_use_services?' do
+      it 'should use services' do
+        expect(@teacher.can_use_services?).to be_truthy
       end
     end
   end
